b4e30c5b86e362d1405892735ba1b82e064b984f
Avinash-Capg <avinash.cg@outlook.com>
2026-01-19 11:57:20 +0530

    Add files via upload

---
+++ Q30_Circular_Garden_Bed/Q30_Circular_Garden_Bed/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        decimal radius = decimal.Parse(Console.ReadLine());
+        decimal pi = 3.1415926535897932384626433m;
+        decimal area = pi * radius * radius;
+        area = Math.Round(area, 2, MidpointRounding.AwayFromZero);
+        Console.WriteLine(area);
+    }
+}

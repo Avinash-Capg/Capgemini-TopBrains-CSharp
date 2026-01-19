6d1d0081675f6e78e7a3469b79c39b5519a4f777
Avinash-Capg <avinash.cg@outlook.com>
2026-01-19 11:43:48 +0530

    Add files via upload

---
+++ Q05_Arithmetic_Expressions/Q05_Arithmetic_Expressions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+        string[] parts = expression.Split(' ');
+
+        if (parts.Length != 3)
+        {
+            Console.WriteLine("Error:InvalidExpression");
+            return;
+        }
+
+        int a, b;
+
+        if (!int.TryParse(parts[0], out a) || !int.TryParse(parts[2], out b))
+        {
+            Console.WriteLine("Error:InvalidNumber");
+            return;
+        }
+
+        string op = parts[1];
+
+        if (op == "+")
+        {
+            Console.WriteLine((a + b).ToString());
+        }
+        else if (op == "-")
+        {
+            Console.WriteLine((a - b).ToString());
+        }
+        else if (op == "*")
+        {
+            Console.WriteLine((a * b).ToString());
+        }
+        else if (op == "/")
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error:DivideByZero");
+            }
+            else
+            {
+                Console.WriteLine((a / b).ToString());
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error:UnknownOperator");
+        }
+    }
+}
---
+++ Q06_Largest_Integer/Q06_Largest_Integer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int largest;
+
+        if (a >= b && a >= c)
+        {
+            largest = a;
+        }
+        else if (b >= a && b >= c)
+        {
+            largest = b;
+        }
+        else
+        {
+            largest = c;
+        }
+
+        Console.WriteLine(largest);
+    }
+}
